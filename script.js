d7092096eafa449be3bbbc3b149e8d8cd9082a83
Eric Ramalho <ericramalho@hotmail.com>
2025-12-02 17:16:12 -0300

    criando a parte de adicionar um icone

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Seleciona os elementos do formulario
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+// Seleciona os elementos da lista
+const expenseList = document.querySelector("ul");
+
 // oniput, fica de olho em tudo que acontece dentro do input. neste medoto estamos pegando o evento do input para formatar o valor
 amount.oninput = () => {
   //obtem o valor atual do input e remove os caracterias nao numericos
@@ -18,4 +25,42 @@ function formatCurrencyBRL(value) {
     currency: "BRL",
   });
   return value;
+}
+
+// Captura o evneto de submit do formulario para obter os valores
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const newExpense = {
+    // Criando um ID
+    id: new Date().getTime(),
+    // Pegando o valor da despesa
+    expense: expense.value,
+    // Pegando o valor da categoria
+    category_id: category.value,
+    // Pegando o nome da categoria
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    // Cria o elemento para adicionar na lista.
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // Cria a categoria do icone
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // Adiciona o intem dentro da lista
+    expenseItem.append(expenseIcon);
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Não foi possivel atualizar a lista de despesas");
+    console.log(error);
+  }
 }
